8786943c358961b541825d8041a688832423f139
sdfasdfasfasf <slk5001@yandex.ru>
2021-06-05 19:25:01 +0300

    work application

--- HomeAccounting.Console/Program.cs
+++ HomeAccounting.Console/Program.cs
@@ -6,7 +6,7 @@ namespace HomeAccounting.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
